3b15d6f6d802b218fcf34a74a143b3145b6bc251
whatsinmyopsec <kevin.power.97.kp@gmail.com>
2022-04-02 23:46:26 +0100

    Delete src directory

--- src/services/queryservice.tsx
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export default {
-    fetchQuery(domain: string, variable: string, Query: any) {
-        return api().get(`${domain}?${variable}=${Query}`, {
-            headers: { "content-type": "application/json" },
-        });
-    }
-}
