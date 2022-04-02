70d44828aaca2f1fd92f168adaa8cededc30cfe7
whatsinmyopsec <kevin.power.97.kp@gmail.com>
2022-04-02 23:47:00 +0100

    Delete tests directory

--- tests/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../src/components/app/App';
-
-test('renders Search text', () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText(/search/i);
-  expect(textElement).toBeInTheDocument();
-});
