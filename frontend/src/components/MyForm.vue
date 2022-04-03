650a89f13a102b40c2d519f9fb40f611b9ed349d
whatsinmyopsec <kevin.power.97.kp@gmail.com>
2022-04-03 19:20:35 +0100

    vite?

---
+++ frontend/src/components/MyForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+    <p>{{ name }}</p>
+        <input type="text" placeholder="Name" v-model="name" />
+        <div>
+            <button 
+                @click="submitForm" 
+                v-bind:disabled="name.length == 0">
+                Submit
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const name = ref("");
+        const submitForm = () => {
+            console.log(name.value);
+        };
+        return {
+            name,
+            submitForm,
+        }
+    }
+    }
+
+</script>
+
+<style>
+input[type="text"] {
+    padding: 10px;
+}
+
+button {
+    margin-top: 10px;
+}
+</style>
